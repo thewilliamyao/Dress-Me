199a537d23e5e4cf2b7f57986f3e26be02f6f64d
etsai7 <etsai7@users.noreply.github.com>
2016-10-16 16:11:44 -0400

    Javadocs

---
+++ Laundry.java
@@ -0,0 +1,20 @@
+/**
+*   The laundry class.
+*/
+public class Laundry extends ClothesContainer{
+
+    /**
+    *   The Laundry constructor
+    */
+    public Laundry(){
+        super();
+    }
+
+    /**
+    * Marks the clean clothes and updates laundry.
+    *   @param c the clothes to mark clean.
+    */
+    public Clothes markClean(Clothes c){
+
+    }
+}
