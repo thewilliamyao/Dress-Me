00398a7c25b77449b696466f334a831d6c42fe82
matthewige <matthewige1@gmail.com>
2016-12-13 11:28:25 -0500

    changed up laundry algorithm, which required changing the db. also did some documentation and cleanup of the Clothes class

--- code/backend/java/src/main/java/com/server/ClothesService.java
+++ code/backend/java/src/main/java/com/server/ClothesService.java
@@ -25,6 +25,7 @@ public class ClothesService {
     private static String[] allAccessories = {"umbrella", "scarf"}; // types of clothing
     private static HashMap<String, Double> lowTempMap; // map to hold defaults for high and low temps
     private static HashMap<String, Double> highTempMap; // map to hold defaults for high and low temps
+    private static HashMap<String, Integer> timesWornMap; // map to hold max number of times each item can be worn
     private static final double TEMP_MAX = 120.0; // limit for temp values
     private static final double TEMP_MIN = -120.0; // limit for temp values
     private static final Logger logger = LoggerFactory.getLogger(ClothesService.class);
@@ -40,7 +41,7 @@ public class ClothesService {
         try (Connection conn = db.open()) {
             String sqlClothes = "CREATE TABLE IF NOT EXISTS clothes (clothes_id INTEGER PRIMARY KEY, " +
                          "                                 user_id INTEGER, type TEXT, specific_type TEXT, number_owned INTEGER, " + 
-                         "                                 number_dirty INTEGER, temp_high DECIMAL, temp_low DECIMAL)" ;
+                         "                                 number_dirty INTEGER, temp_high DECIMAL, temp_low DECIMAL, times_worn INTEGER, max_times_worn INTEGER)" ;
             conn.createQuery(sqlClothes).executeUpdate();
             String sqlClothesId = "SELECT MAX(clothes_id) FROM clothes";
             Integer latestClothes = conn.createQuery(sqlClothesId)
@@ -51,6 +52,8 @@ public class ClothesService {
                 .addColumnMapping("number_dirty", "numberDirty")
                 .addColumnMapping("temp_high", "tempHigh")
                 .addColumnMapping("temp_low", "tempLow")
+                .addColumnMapping("times_worn", "timesWorn")
+                .addColumnMapping("max_times_worn", "maxTimesWorn")
                 .executeAndFetchFirst(Integer.class);
             if (latestClothes != null) {
                 clothesCounter = latestClothes.intValue() + 1;
@@ -58,42 +61,56 @@ public class ClothesService {
                 clothesCounter = 0;
             }
             // create default mapping of temperatures
-            // TODO: turn this into a static DB maybe?
             lowTempMap = new HashMap<String, Double>();
             highTempMap = new HashMap<String, Double>();
+            timesWornMap = new HashMap<String, Integer>();
             // shirts
             lowTempMap.put("tank_top", new Double(80));
             highTempMap.put("tank_top", new Double(TEMP_MAX));
+            timesWornMap.put("tank_top", new Integer(1));
             lowTempMap.put("t_shirt", new Double(TEMP_MIN));
             highTempMap.put("t_shirt", new Double(TEMP_MAX));
+            timesWornMap.put("t_shirt", new Integer(1));
             lowTempMap.put("long_sleeve", new Double(TEMP_MIN));
             highTempMap.put("long_sleeve", new Double(80));
+            timesWornMap.put("long_sleeve", new Integer(1));
             // pants
             lowTempMap.put("shorts", new Double(60));
             highTempMap.put("shorts", new Double(120));
+            timesWornMap.put("shorts", new Integer(3));
             lowTempMap.put("long_pants", new Double(TEMP_MIN));
             highTempMap.put("long_pants", new Double(80));
+            timesWornMap.put("long_pants", new Integer(3));
             // outerwear
             lowTempMap.put("hoodie", new Double(50));
             highTempMap.put("hoodie", new Double(80));
+            timesWornMap.put("hoodie", new Integer(-1));
             lowTempMap.put("windbreaker", new Double(50));
             highTempMap.put("windbreaker", new Double(TEMP_MAX));
+            timesWornMap.put("windbreaker", new Integer(-1));
             lowTempMap.put("sweater", new Double(50));
             highTempMap.put("sweater", new Double(80));
+            timesWornMap.put("sweater", new Integer(-1));
             lowTempMap.put("winter_coat", new Double(TEMP_MIN));
             highTempMap.put("winter_coat", new Double(40));
+            timesWornMap.put("winter_coat", new Integer(-1));
             // accessories
             lowTempMap.put("umbrella", new Double(TEMP_MIN));
             highTempMap.put("umbrella", new Double(TEMP_MAX));
+            timesWornMap.put("umbrella", new Integer(-1));
             lowTempMap.put("scarf", new Double(TEMP_MIN));
             highTempMap.put("scarf", new Double(50));
+            timesWornMap.put("scarf", new Integer(-1));
             // shoes
             lowTempMap.put("boots", new Double(TEMP_MIN));
             highTempMap.put("boots", new Double(40));
+            timesWornMap.put("boots", new Integer(-1));
             lowTempMap.put("sandals", new Double(80));
             highTempMap.put("sandals", new Double(TEMP_MAX));
+            timesWornMap.put("sandals", new Integer(-1));
             lowTempMap.put("shoes", new Double(TEMP_MIN));
             highTempMap.put("shoes", new Double(TEMP_MAX));
+            timesWornMap.put("shoes", new Integer(-1));
         }
     }
 
@@ -118,8 +135,8 @@ public class ClothesService {
     * @param currUserId the id corresponding to the user.
     */
     public static void addClothing(String type, String specificType [], int currUserId) throws ClothesServiceException {
-        String sqlClothes = "INSERT INTO clothes (clothes_id, user_id, type, specific_type, number_owned, number_dirty, temp_high, temp_low)" + 
-                "                   VALUES (:clothesId, :userId, :type, :specificType, :numberOwned, :numberDirty, :tempHigh, :tempLow) ";
+        String sqlClothes = "INSERT INTO clothes (clothes_id, user_id, type, specific_type, number_owned, number_dirty, temp_high, temp_low, times_worn, max_times_worn)" + 
+                "                   VALUES (:clothesId, :userId, :type, :specificType, :numberOwned, :numberDirty, :tempHigh, :tempLow, :timesWorn, :maxTimesWorn) ";
 
         for(String s : specificType) {
             try (Connection conn = db.open()) {
@@ -140,6 +157,8 @@ public class ClothesService {
                         .addParameter("numberDirty", 0)
                         .addParameter("tempHigh", highTempMap.containsKey(s) ? highTempMap.get(s) : TEMP_MAX)
                         .addParameter("tempLow", lowTempMap.containsKey(s) ? lowTempMap.get(s) : TEMP_MIN)
+                        .addParameter("timesWorn", 0)
+                        .addParameter("maxTimesWorn", timesWornMap.containsKey(s) ? timesWornMap.get(s).intValue() : -1)
                     .executeUpdate();
             } catch (Sql2oException ex) {
                 logger.error("ClothesService.addClothing: Failed to add new clothing entry", ex);
@@ -166,6 +185,8 @@ public class ClothesService {
                     .addColumnMapping("number_dirty", "numberDirty")
                     .addColumnMapping("temp_high", "tempHigh")
                     .addColumnMapping("temp_low", "tempLow")
+                    .addColumnMapping("times_worn", "timesWorn")
+                    .addColumnMapping("max_times_worn", "maxTimesWorn")
                     .addParameter("userId", id)
                     .executeAndFetch(Clothes.class);
             return allClothes;
@@ -195,6 +216,8 @@ public class ClothesService {
                     .addColumnMapping("number_dirty", "numberDirty")
                     .addColumnMapping("temp_high", "tempHigh")
                     .addColumnMapping("temp_low", "tempLow")
+                    .addColumnMapping("times_worn", "timesWorn")
+                    .addColumnMapping("max_times_worn", "maxTimesWorn")
                     .addParameter("userId", id)
                     .executeAndFetch(Clothes.class);
 
@@ -229,6 +252,8 @@ public class ClothesService {
                     .addColumnMapping("number_dirty", "numberDirty")
                     .addColumnMapping("temp_high", "tempHigh")
                     .addColumnMapping("temp_low", "tempLow")
+                    .addColumnMapping("times_worn", "timesWorn")
+                    .addColumnMapping("max_times_worn", "maxTimesWorn")
                     .addParameter("userId", id)
                     .executeAndFetch(Clothes.class);
 
@@ -269,6 +294,8 @@ public class ClothesService {
                 .addColumnMapping("number_dirty", "numberDirty")
                 .addColumnMapping("temp_high", "tempHigh")
                 .addColumnMapping("temp_low", "tempLow")
+                .addColumnMapping("times_worn", "timesWorn")
+                .addColumnMapping("max_times_worn", "maxTimesWorn")
                 .addParameter("specificType", clothesType)
                 .addParameter("numberOwned", number)
                 .addParameter("userId", currId)
@@ -279,6 +306,50 @@ public class ClothesService {
         }
 
         return getClothesMap(currId);
+    }
+
+    /**
+    * Helper function to evaluate if an item should be marked dirty. Does the appropriate updates as necessary.
+    * Also updates the db.
+    * @param dirtyMap a reference to a hashmap with the number of dirty items
+    * @param timesWornMap a reference to a hashmap with the number of times an item was worn
+    * @param maxTimesWornMap a reference to a hashmap with the number of tiems an item can be worn
+    */
+    private void evaluateDirty(int currId, HashMap<String, Integer> dirtyMap, HashMap<String, Integer> timesWornMap, HashMap<String, Integer> maxTimesWornMap, String item) throws ClothesServiceException {
+        // if the key is not present or the max times worn is -1, we can skip this item.
+        if (!maxTimesWornMap.containsKey(item) || maxTimesWornMap.get(item) == -1) {
+            return;
+        }
+        if (timesWornMap.get(item) + 1 == maxTimesWornMap.get(item)) {
+            timesWornMap.put(item, 0);
+            dirtyMap.put(item, dirtyMap.get(item) + 1);
+        } else {
+            timesWornMap.put(item, timesWornMap.get(item) + 1);
+        }
+
+        // regardless of the outcome, update the dirty map
+        String updateDirty = "UPDATE clothes SET number_dirty = :numberDirty, times_worn = :timesWorn WHERE (user_id = :userId AND specific_type = :specificType)";
+        try (Connection conn = db.open()) {
+            conn.createQuery(updateDirty)
+                .addColumnMapping("user_id", "userId")
+                .addColumnMapping("clothes_id", "clothesId")
+                .addColumnMapping("type", "type")
+                .addColumnMapping("specific_type", "specificType")
+                .addColumnMapping("number_owned", "numberOwned")
+                .addColumnMapping("number_dirty", "numberDirty")
+                .addColumnMapping("temp_high", "tempHigh")
+                .addColumnMapping("temp_low", "tempLow")
+                .addColumnMapping("times_worn", "timesWorn")
+                .addColumnMapping("max_times_worn", "maxTimesWorn")
+                .addParameter("specificType", item)
+                .addParameter("numberDirty", dirtyMap.get(item))
+                .addParameter("timesWorn", timesWornMap.get(item))
+                .addParameter("userId", currId)
+                .executeUpdate();
+        } catch (Sql2oException ex) {
+            logger.error("ClothesService.evaluateDirty: Failed to update clothes", ex);
+            throw new ClothesServiceException("ClothesService.evaluate: Failed to update clothes", ex);
+        }
     }
 
     /**
@@ -299,6 +370,8 @@ public class ClothesService {
         // obtain the current count of dirty items
         String sqlTops = "SELECT * FROM clothes WHERE (user_id = :userId AND type = :type)";
         HashMap<String, Integer> dirtyMap = new HashMap<String, Integer>();
+        HashMap<String, Integer> timesWornMap = new HashMap<String, Integer>();
+        HashMap<String, Integer> maxTimesWornMap = new HashMap<String, Integer>();
         try (Connection conn = db.open()) {
             List<Clothes> allClothes = 
                 conn.createQuery(sqlTops)
@@ -310,11 +383,15 @@ public class ClothesService {
                     .addColumnMapping("number_dirty", "numberDirty")
                     .addColumnMapping("temp_high", "tempHigh")
                     .addColumnMapping("temp_low", "tempLow")
+                    .addColumnMapping("times_worn", "timesWorn")
+                    .addColumnMapping("max_times_worn", "maxTimesWorn")
                     .addParameter("userId", currId)
                     .addParameter("type", "top")
                     .executeAndFetch(Clothes.class);
             for (Clothes c : allClothes) {
                 dirtyMap.put(c.getSpecificType(), c.getNumberDirty());
+                timesWornMap.put(c.getSpecificType(), c.getTimesWorn());;
+                maxTimesWornMap.put(c.getSpecificType(), c.getMaxTimesWorn());
             }
 
         } catch (Sql2oException ex) {
@@ -322,29 +399,12 @@ public class ClothesService {
             throw new ClothesServiceException("ClothesService.markDirty: Failed to get clothes map", ex);
         }
 
-        // update in the local map
-        dirtyMap.put(top, (dirtyMap.get(top) + 1)); 
-        // update the top we need to
-        String updateDirty = "UPDATE clothes SET number_dirty = :numberDirty WHERE (user_id = :userId AND specific_type = :specificType)";
-
-        try (Connection conn = db.open()) {
-            conn.createQuery(updateDirty)
-                .addColumnMapping("user_id", "userId")
-                .addColumnMapping("clothes_id", "clothesId")
-                .addColumnMapping("type", "type")
-                .addColumnMapping("specific_type", "specificType")
-                .addColumnMapping("number_owned", "numberOwned")
-                .addColumnMapping("number_dirty", "numberDirty")
-                .addColumnMapping("temp_high", "tempHigh")
-                .addColumnMapping("temp_low", "tempLow")
-                .addParameter("specificType", top)
-                .addParameter("numberDirty", dirtyMap.get(top))
-                .addParameter("userId", currId)
-                .executeUpdate();
-        } catch (Sql2oException ex) {
-            logger.error("ClothesService.markDirty: Failed to update clothes", ex);
-            throw new ClothesServiceException("ClothesService.markDirty: Failed to update clothes", ex);
-        }
+        // update the value as necessary
+        evaluateDirty(currId, dirtyMap, timesWornMap, maxTimesWornMap, top);
+        evaluateDirty(currId, dirtyMap, timesWornMap, maxTimesWornMap, pants);
+        evaluateDirty(currId, dirtyMap, timesWornMap, maxTimesWornMap, footwear);
+        evaluateDirty(currId, dirtyMap, timesWornMap, maxTimesWornMap, accessory);
+        evaluateDirty(currId, dirtyMap, timesWornMap, maxTimesWornMap, outerwear);
 
         // then check for each item, if there is less than 30% clean, return true, to signal should do laundry
         HashMap<String, Integer> ownedMap = getClothesMap(currId);
@@ -363,7 +423,7 @@ public class ClothesService {
     */
     public void markClean(int currId) throws ClothesServiceException {
         // set all dirty fields to be 0
-        String updateDirty = "UPDATE clothes SET number_dirty = :numberDirty WHERE user_id = :userId";
+        String updateDirty = "UPDATE clothes SET number_dirty = :numberDirty, times_worn = :timesWorn WHERE user_id = :userId";
         
         try (Connection conn = db.open()) {
             conn.createQuery(updateDirty)
@@ -375,6 +435,9 @@ public class ClothesService {
                 .addColumnMapping("number_dirty", "numberDirty")
                 .addColumnMapping("temp_high", "tempHigh")
                 .addColumnMapping("temp_low", "tempLow")
+                .addColumnMapping("times_worn", "timesWorn")
+                .addColumnMapping("max_times_worn", "maxTimesWorn")
+                .addParameter("timesWorn", 0)
                 .addParameter("numberDirty", 0)
                 .addParameter("userId", currId)
                 .executeUpdate();
